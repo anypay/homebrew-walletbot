8ba3a958642cc09a8152a1b690abc549d77b7fe5
My CTO <mycto@proton.me>
2024-03-01 10:33:59 +0100

    Correctly install anypay-go as anypay on mac with ARM chip

--- README.md
+++ README.md
@@ -2,5 +2,23 @@
 
 ```
 brew tap anypay/anypay
+
 brew install anypay
+
+brew upgrade anypay
 ```
+
+## Updating This Repo When A New Build Version Is Published
+
+(Eventually this shall be automated)
+
+After releasing a new version to github the latest version should be included here.
+
+Download the latest verion's .tar.gz file for both ARM and AMD, then run:
+
+`shasum -256 path_to_build.tar.gz`
+
+Update `anypay.rb` with the latest BUILD_VERSION variable and manually set both its `sha256`
+with the correct hashes.
+
+Once this update is pushed to github, clients will need to run `brew upgrade anypay`
--- anypay.rb
+++ anypay.rb
@@ -1,17 +1,19 @@
+BUILD_VERSION = "0.0.7"
+
 class Anypay < Formula
   desc "Anypay Server for Web3 Payments"
   homepage "https://github.com/anypay/anypay-go"
-  url "https://github.com/anypay/anypay-go/releases/download/0.0.7/anypay-go-0.0.7-darwin-amd64.tar.gz"
+  url "https://github.com/anypay/anypay-go/releases/download/#{BUILD_VERSION}/anypay-go-#{BUILD_VERSION}-darwin-amd64.tar.gz"
   sha256 "00390ce32d58c1c8e43945d8dd587265"
 
   # For ARM architecture
   on_arm do
-    url "https://github.com/anypay/anypay-go/releases/download/0.0.7/anypay-go-0.0.7-darwin-arm64.tar.gz"
-    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    url "https://github.com/anypay/anypay-go/releases/download/#{BUILD_VERSION}/anypay-go-#{BUILD_VERSION}-darwin-arm64.tar.gz"
+    sha256 "0a348ddf4b4e8d77b193816accfc0031420b3f88aa62593115e1276b9ac0790d"
   end
 
   def install
-    bin.install "anypay"
+    bin.install "anypay-go" => "anypay"
   end
 
   test do
